2b69f7700a18d223d69e594738e07f81ce0e0d2b
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-23 17:20:48 +0200

    ese finito

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppMain from './components/AppMain.vue'
+
+import { store } from './data/store';
+import axios from 'axios';
+
+
+
 
 export default {
   components: {
     AppTitle,
+    AppMain,
 
   },
   data() {
     return {
+      store
     }
   },
   mounted() {
+    axios.get(this.store.urlAPI).then(r => {
+      store.cardElement = r.data.data;
+    }).catch(errore => {
+      console.log("Errore di chiamata")
+    })
   }
 }
 </script>
@@ -20,9 +34,15 @@ export default {
     <AppTitle />
   </header>
 
-  <main></main>
+  <main>
+    <AppMain />
+  </main>
 
   <footer></footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  background-color: #C68636;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+import { store } from '../data/store';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container p-5 ">
+        <div class="row d-flex justify-content-center">
+            <div v-for="card in store.cardElement" class="col-12 col-lg-2">
+                <div class="card cardBody ">
+                    <img :src="card.card_images[0].image_url">
+                    <div class="card-body minmaxHeight">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-bottom border-black">{{ card.name }}</li>
+                            <li class="list-group-item border-bottom fw-lighter">{{ card.type }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card img {
+    width: 100%;
+}
+
+.minmaxHeight {
+    min-height: 200px;
+    background-color: #D48F38;
+}
+
+.cardBody {
+    padding: 15px;
+    width: 14rem;
+}
+</style>
