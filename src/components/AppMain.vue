99916d27215f2ffc790b6254aaefbb717b904adc
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-23 22:49:40 +0200

    fix code

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,23 +12,18 @@ export default {
 </script>
 
 <template>
-    <div class="container p-5 ">
+    <div class="container p-5">
         <div class="row">
             <div class="col-12 bg-black text-white py-3 mb-2">
                 <span class="fw-bold">Found 50 cards</span>
             </div>
         </div>
-        <div class="row gap-4 d-flex">
-            <div v-for="card in store.cardElement" class="col-2">
-                <div class="card cardBody ">
-                    <img :src="card.card_images[0].image_url">
-                    <div class="card-body minmaxHeight">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item border-bottom border-black">{{ card.name }}</li>
-                            <li class="list-group-item border-bottom fw-lighter">{{ card.type }}</li>
-                        </ul>
-                    </div>
-                </div>
+        <div class="container h-100 row gap-3 bg-white justify-content-between ">
+            <div v-for="card in store.cardElement"
+                class="col-2 bgColorBody text-center d-flex flex-column justify-content-between p-0">
+                <img :src="card.card_images[0].image_url" class="img-fluid">
+                <div class=" border-bottom border-black text-white">{{ card.name }}</div>
+                <div class=" border-top-0 fw-lighter text-white">{{ card.type }}</div>
             </div>
         </div>
     </div>
@@ -37,11 +32,9 @@ export default {
 <style scoped>
 .minmaxHeight {
     min-height: 200px;
-    background-color: #D48F38;
 }
 
-.cardBody {
-    padding: 15px;
-    width: 14rem;
+.bgColorBody {
+    background-color: #D48F38;
 }
 </style>
